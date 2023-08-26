b4934b85be76a6bb46ea2ed45f325025514a72dd
code3743 <jota.lopez@correounivalle.edu.co>
2023-08-26 10:36:20 -0500

    feat(ListaFactoriales): Agregar implementación de objListaFactoriales

--- app/src/main/scala/introduccion/ListaFactoriales.scala
+++ app/src/main/scala/introduccion/ListaFactoriales.scala
@@ -3,9 +3,11 @@ package introduccion
 
 class ListaFactoriales {
     def objListaFactoriales(n : Int): List[BigInt] = {
-        var listaFactoriales : List[BigInt] = List()
-        val objFactorial: Factorial = new Factorial()
-        //Complete el código
-        throw new UnsupportedOperationException("No implementado aún")
+        var listaFactoriales : List[BigInt] = List();
+        val objFactorial: Factorial = new Factorial();
+        for(i <- 0 until (n + 1)){
+            listaFactoriales = listaFactoriales.appended(objFactorial.calculateFactorial(i));
+        }
+        return listaFactoriales;
     }
 }
